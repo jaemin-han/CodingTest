62f02f0573b042599f6cd3c2425c7b8b2bb13bf1
jaemin-han <duddjakgkrl@gmail.com>
2022-07-27 01:09:18 +0900

    split 매소드가 비어 있는 문자열에 대해서 어떻게 작동하는지 알아보자

---
+++ level6/1152_단어의 개수.py
@@ -0,0 +1,25 @@
+"""
+문자열은 공백으로 시작하거나 끝날 수 있다.
+"""
+# string = input().strip().split(' ')
+string = input().strip()
+
+print(len(string))
+
+print(f">{string}<")
+print(string)
+if string:
+    print("이거안비어있음")
+"""
+
+"""
+# string = input()
+
+# count = 0
+# if string[0] != ' ':
+#     count = 1
+
+# for i, char in enumerate(string):
+#     if i != len(string) - 1 and char == ' ' and string[i + 1] != ' ':
+#         count += 1
+# print(count)
