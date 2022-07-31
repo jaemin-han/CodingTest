96ad65201fe19e74ea1fd88a1ec0eebf119bc4ba
jaemin-han <duddjakgkrl@gmail.com>
2022-08-01 01:55:24 +0900

    또 틀렸네 ,,ㅠ

--- level7/2869-달팽이는_올라가고_싶다.py
+++ level7/2869-달팽이는_올라가고_싶다.py
@@ -4,9 +4,33 @@
 그리고 첫날에 다 오르지 못하는 경우,
 일단 달팽이가 밤을 지나고 아침이 되었을 때 남은 높이가 a 이하라면 당일 낮에 다 오를 수 있다.
 따라서 (v - a) // (a - b) + 1 이 답이 아닐까?
+
+달팽이가 n날 밤을 지나고 남은 높이가 a 이하라면..인데 n날 밤이란?
+일단 v 를 (a - b) 로 나눈 나머지가 0이라면, 그 몫보다 하나 작은 값이거나
+아니면 그 몫
 """
+# def main():
+#     a, b, v = map(int, input().split())
+#     if a >= v:
+#         print(1)
+#         return
+#     if v % (a - b) == 0:
+#         quotient = v // (a - b) - 1
+#     else:
+#         quotient = v // (a - b)
+#     if v - (quotient - 1) * (a - b) <= a:
+#         print(quotient)
+#     else:
+#         print(quotient + 1)
+    
 def main():
     a, b, v = map(int, input().split())
-    print((v - a) // (a - b) + 1)
-    
-main()
+    if a >= v:
+        print(1)
+    elif v - a <= a - b:
+        print(2)
+    else:
+        print((v - a) // (a - b) + 1)
+
+while True:
+    main()
