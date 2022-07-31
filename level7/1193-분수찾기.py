8ae52ac71b893933c90fb7e87b2d703a91f5a1f3
jaemin-han <duddjakgkrl@gmail.com>
2022-08-01 00:58:34 +0900

    문제를 똑바로 읽자 ^^ solved!

--- level7/1193-분수찾기.py
+++ level7/1193-분수찾기.py
@@ -1,6 +1,8 @@
 """
 i 번째 껍질에는 i 개의 원소가 들어있다. 일단 X 번째 분수를 구하기 위해서는 얘가 몇 번쨰 껍질에 속한지 부터 구한다.
 i 번째 껍질은 1 ~ i - 1 을 합한 수 보다 크고 1 ~ i 를 합한 수 보다 작거나 같다.
+
+지그재그로 한다는 걸 까먹었다.. 다시 지그인지, 재그인지 체크하는 부분이 필요함
 """
 def main():
     n = int(input())
@@ -8,9 +10,12 @@ def main():
     while n > sum(range(orbital + 1)):
         orbital += 1
     n -= sum(range(orbital)) # 이제 orbital이 껍질 수가 되고, n은 껍질에서의 몇 번째 수 인지를 나타냄
-    numerator = n
-    denominator = orbital - n + 1
+    if orbital % 2 == 0:
+        numerator = n
+        denominator = orbital - n + 1
+    else:
+        denominator = n
+        numerator = orbital - n + 1
     print(numerator, '/', denominator, sep='')
 
-while True:
-    main()
+main()
